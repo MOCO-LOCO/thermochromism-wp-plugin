f1ae0ae8a96d730fd2d77c2bb10ebc820a09170e
akrfranko <franko@akr.club>
2014-11-22 07:59:47 -0500

    First.

---
+++ thermochromism.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Thermochromism
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: thermochromism
+Domain Path: /languages
+*/
+
